2addadae8e31d33f5e0eb792122d2939bd1c7648
Trevor <alutiiq@comcast.net>
2020-07-14 13:23:06 -0700

    should work

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log('Hello world');
+
+let count =0;
+let counter = document.getElementById('counter');
+
+function increase(){
+    count++;
+    counter.innerText = count;
+};
+
+function decrease(){
+    count--;
+    counter.innerText = count;
+};
+
+function reset(){
+    count = 0;
+    counter.innerHTML = "<mark>" + count + "</mark>"
+};
+
+function selectTheme(theme){
+    let body = document.getElementsByTagName('body')
+    body[0].className = theme;
+    
+    let main = document.querySelector('main');
+    main.className = theme;
+
+    let buttons = document.getElementsByTagName('button')
+    for( let i = 0; i < buttons.length; i++){
+        buttons[i].className = theme
+    }
+}
